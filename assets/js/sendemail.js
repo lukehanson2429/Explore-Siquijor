7999915a540f74d20ea1dcb1ae8604e1f738aacd
lukehanson2429 <lukehanson2429@gmail.com>
2021-03-18 13:20:05 +0000

    project_request key within function object renamed to message to tie in with my project

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -2,7 +2,7 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "exploresiquijor", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.message.value
+        "message": contactForm.message.value
     })
     .then(
         function(response) {
