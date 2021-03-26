1109ca1293a35df9ed2f501aba766db9f52afbc5
lukehanson2429 <lukehanson2429@gmail.com>
2021-03-26 15:55:50 +0000

    mouse enter & mouse leave function added targeteting .bt-custom-ed which changes color

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(".btn-custom-ed").mouseenter(function() {
+        $(".tp-col").css("color", "rgba(240, 117, 24, 1)");
+   });
+
+$(".btn-custom-ed").mouseleave(function() {
+        $(".tp-col").css("color", "white");
+   });
