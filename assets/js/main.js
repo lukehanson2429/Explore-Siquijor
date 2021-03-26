91919ebe70b8fd4b75e5b27f07d8b4a7d7c3447c
lukehanson2429 <lukehanson2429@gmail.com>
2021-03-26 16:05:08 +0000

    mouseenter & mouse leave function added targeting .btn-custom-attr class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 $(".btn-custom-ed").mouseenter(function() {
-        $(".tp-col").css("color", "rgba(240, 117, 24, 1)");
+        $("#top-bg-eat").css("color", "rgba(240, 117, 24, 1)");
    });
 
 $(".btn-custom-ed").mouseleave(function() {
-        $(".tp-col").css("color", "white");
+        $("#top-bg-eat").css("color", "white");
+   });
+
+$(".btn-custom-attr").mouseenter(function() {
+        $("#top-bg-attr").css("color", "rgba(171, 72, 196, 1)");
+   });
+
+$(".btn-custom-attr").mouseleave(function() {
+        $("#top-bg-attr").css("color", "white");
    });
