bb26827ba55e4d36e06cc11cdb4fc3fed1a213c2
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-01 13:46:06 +0000

    btn click show & hide functions added to show .tp-hidden & hide .btn-scroll at 5000ms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var myContainer = $('.scroll-container')
+
 $(".btn-toptip").click(function() {
   $("#tp-Content-2").toggle(3000);
 });
@@ -6,11 +8,17 @@ $(".btn-toptip").click(function() {
   $("#tp-Content-1").toggle(3000);
 });
 
-var myContainer = $('.scroll-container')
+$(".btn-scroll").click(function() {
+  $(".tp-hidden").show(5000);
+});
+
+$(".btn-scroll").click(function() {
+  $(".btn-scroll").hide(5000);
+});
 
 $(myContainer).on('click', '.btn-scroll', function () {
-    var scrollTo = $('#scroll-bottom');
+    var scrollTo = $('.scroll-next');
     myContainer.animate({
         scrollTop: scrollTo.offset().top - myContainer.offset().top + myContainer.scrollTop()
-    }, 20000);
+    }, 5000);
 });
