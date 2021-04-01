1f17fc0beefd7d8d616d191dac33b8e044f0d055
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-01 15:01:36 +0000

    function added which hides Nav when not scrolling, changed to sticky in css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
 var topPicksScroll = $('.scroll-container')
 
+
+// Used from tutorial and adjusted to suit my project- https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("hideNav").style.top = "0";
+  } else {
+    document.getElementById("hideNav").style.top = "-82px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+$(".btn-custom").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#map-section").offset().top
+    }, 2000);
+});
+
 $(".btn-toptip").click(function() {
   $("#tp-Content-2").toggle(3000);
 });
@@ -14,12 +33,6 @@ $(".btn-scroll").click(function() {
 
 $(".btn-scroll").click(function() {
   $(".btn-scroll").hide(2000);
-});
-
-$(".btn-custom").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#map-section").offset().top
-    }, 2000);
 });
 
 $(topPicksScroll).on('click', '.btn-scroll', function () {
