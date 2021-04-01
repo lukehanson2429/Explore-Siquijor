05dfd29b036f6e333113374ed4d192d41d1cb704
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-01 13:27:35 +0000

    scroll container function added which scrolls to bottom of top picks section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,12 @@ $(".btn-toptip").click(function() {
 $(".btn-toptip").click(function() {
   $("#tp-Content-1").toggle(3000);
 });
+
+var myContainer = $('.scroll-container')
+
+$(myContainer).on('click', '.btn-scroll', function () {
+    var scrollTo = $('#scroll-bottom');
+    myContainer.animate({
+        scrollTop: scrollTo.offset().top - myContainer.offset().top + myContainer.scrollTop()
+    }, 20000);
+});
