e96c8282d1cdf04dd1632136bff70b9c7774a9e3
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-01 12:29:55 +0000

    top ticks section all within one section of scrollable container & jquery mouse over functions removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-$(".btn-custom-fd").mouseenter(function() {
-        $("#top-bg-fd").css("color", "rgba(240, 117, 24, 1)");
-   });
-
-$(".btn-custom-fd").mouseleave(function() {
-        $("#top-bg-fd").css("color", "white");
-   });
-
-$(".btn-custom-attr").mouseenter(function() {
-        $("#top-bg-attr").css("color", "rgba(171, 72, 196, 1)");
-   });
-
-$(".btn-custom-attr").mouseleave(function() {
-        $("#top-bg-attr").css("color", "white");
-   });
-
-$(".btn-custom-hos").mouseenter(function() {
-        $("#top-bg-hos").css("color", "rgba(21, 209, 177, 1)");
-   });
-
-$(".btn-custom-hos").mouseleave(function() {
-        $("#top-bg-hos").css("color", "white");
-   });
-
 $(".btn-toptip").click(function() {
   $("#tp-Content-2").toggle(3000);
 });
