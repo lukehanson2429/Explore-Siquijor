a883688fe23596014ab3a4263032a35d2052bbca
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-01 14:27:12 +0000

    scroll animation function added targeting .btn-custom to scroll to top of map section div at 2000ms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var myContainer = $('.scroll-container')
+var topPicksScroll = $('.scroll-container')
 
 $(".btn-toptip").click(function() {
   $("#tp-Content-2").toggle(3000);
@@ -9,16 +9,22 @@ $(".btn-toptip").click(function() {
 });
 
 $(".btn-scroll").click(function() {
-  $(".tp-hidden").show(5000);
+  $(".tp-hidden").show(2000);
 });
 
 $(".btn-scroll").click(function() {
-  $(".btn-scroll").hide(5000);
+  $(".btn-scroll").hide(2000);
 });
 
-$(myContainer).on('click', '.btn-scroll', function () {
-    var scrollTo = $('.scroll-next');
-    myContainer.animate({
-        scrollTop: scrollTo.offset().top - myContainer.offset().top + myContainer.scrollTop()
-    }, 5000);
+$(".btn-custom").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#map-section").offset().top
+    }, 2000);
+});
+
+$(topPicksScroll).on('click', '.btn-scroll', function () {
+    var scrollTo = $(this);
+    topPicksScroll.animate({
+        scrollTop: scrollTo.offset().top - topPicksScroll.offset().top + topPicksScroll.scrollTop()
+    }, 2000);
 });
