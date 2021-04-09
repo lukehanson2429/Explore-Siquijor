f0779a24fc6ba4cc741d8019214fd1e8ab89c095
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-09 12:50:18 +0000

    Function to reset value if input name/email & text area of message on submit

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -2,7 +2,6 @@
 emailjs.init("user_05b6DcHp56rlXYPEUQP1y");
 })();
 
-
 function sendMail(contactForm) {
     emailjs.send("gmail", "exploresiquijor", {
         "from_name": contactForm.name.value,
@@ -11,10 +10,19 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            alert("SUCCESS", response);
+            alert("Email Sent!", response);
         },
         function(error) {
-            alert("FAILED", error);
+            alert("Failed to Send!", error);
         });
         return false;
 }
+
+// Function to reset value if input name/email & text area of message on submit (code altered to suit my project)- https://stackoverflow.com/questions/35492054/how-to-reset-input-field-jquery
+$(document).ready(function(){
+    $('form[name=contactForm]').on('submit', function() {
+          $('input[name=name]').val("")
+          $('input[name=emailaddress]').val("")
+          $('textarea[name=message]').val("")
+        });
+});
