a38903262b6891958bfaba1df529157b06d4c96e
lukehanson2429 <lukehanson2429@gmail.com>
2021-04-12 13:54:08 +0000

    all code formatted, html run through validator and any bugs fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,109 @@
-var topPicksScroll = $('.scroll-container')
+var topPicksScroll = $(".scroll-container");
 const speed = 1000;
 
 // Navbar Collapse on Scroll (adjusted to suit my project)- https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("hideNav").style.top = "0";
-  } else {
-    document.getElementById("hideNav").style.top = "-82px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("hideNav").style.top = "0";
+    } else {
+        document.getElementById("hideNav").style.top = "-82px";
+    }
+    prevScrollpos = currentScrollPos;
+};
 
 // Function to collapse Navbar on mobile device on Click
-$('.navbar-nav>li>a').click(function(){
-    $('.navbar-collapse').collapse('hide');
+$(".navbar-nav>li>a").click(function () {
+    $(".navbar-collapse").collapse("hide");
 });
 
-  
-    // Smooth Scrolling adjusted to suit my project - https://documentation.unbounce.com/hc/en-us/articles/360022719471-Adding-Smooth-Scrolling-Links-and-Buttons-for-Anchor-Links
+// Smooth Scrolling adjusted to suit my project - https://documentation.unbounce.com/hc/en-us/articles/360022719471-Adding-Smooth-Scrolling-Links-and-Buttons-for-Anchor-Links
 
-    $('a[href*="#"]')
-      .filter((i, a) => a.getAttribute('href').startsWith('#') || a.href.startsWith(`${location.href}#`))
-      .unbind('click.smoothScroll')
-      .bind('click.smoothScroll', event => {
-        const targetId = event.currentTarget.getAttribute('href').split('#')[1];
+$('a[href*="#"]')
+    .filter((i, a) => a.getAttribute("href").startsWith("#") || a.href.startsWith(`${location.href}#`))
+    .unbind("click.smoothScroll")
+    .bind("click.smoothScroll", (event) => {
+        const targetId = event.currentTarget.getAttribute("href").split("#")[1];
         const targetElement = document.getElementById(targetId);
 
         if (targetElement) {
-          event.preventDefault();
-          $('html, body').animate({ scrollTop: $(targetElement).offset().top }, speed);
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: $(targetElement).offset().top }, speed);
         }
-      });
-  
+    });
 
-$(".btn-custom").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#map-section").offset().top
-    }, speed);
+$(".btn-custom").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $("#map-section").offset().top,
+        },
+        speed
+    );
 });
 
-$(".btn-custom-attr").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#map-section").offset().top
-    }, speed);
+$(".btn-custom-attr").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $("#map-section").offset().top,
+        },
+        speed
+    );
 });
 
-$(".btn-custom-hos").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#map-section").offset().top
-    }, speed);
+$(".btn-custom-hos").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $("#map-section").offset().top,
+        },
+        speed
+    );
 });
 
-$(".btn-custom-fd").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#map-section").offset().top
-    }, speed);
+$(".btn-custom-fd").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $("#map-section").offset().top,
+        },
+        speed
+    );
 });
 
-$(".btn-custom-tt").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#map-section").offset().top
-    }, speed);
+$(".btn-custom-tt").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $("#map-section").offset().top,
+        },
+        speed
+    );
 });
 
-$(".btn-toptip").click(function() {
-  $("#tp-Content-2").toggle(speed);
+$(".btn-toptip").click(function () {
+    $("#tp-Content-2").toggle(speed);
 });
 
-$(".btn-toptip").click(function() {
-  $("#tp-Content-1").toggle(speed);
+$(".btn-toptip").click(function () {
+    $("#tp-Content-1").toggle(speed);
 });
 
-$(".btn-toptip").click(function() {
-  $("#top-tip-img").toggle(speed);
+$(".btn-toptip").click(function () {
+    $("#top-tip-img").toggle(speed);
 });
 
-$(".btn-scroll").click(function() {
-  $(".tp-hidden").show(speed);
+$(".btn-scroll").click(function () {
+    $(".tp-hidden").show(speed);
 });
 
-$(".btn-scroll").click(function() {
-  $(".btn-scroll").hide(speed);
+$(".btn-scroll").click(function () {
+    $(".btn-scroll").hide(speed);
 });
 
-$(topPicksScroll).on('click', '.btn-scroll', function () {
+$(topPicksScroll).on("click", ".btn-scroll", function () {
     var scrollTo = $(this);
-    topPicksScroll.animate({
-        scrollTop: scrollTo.offset().top - topPicksScroll.offset().top + topPicksScroll.scrollTop()
-    }, speed);
+    topPicksScroll.animate(
+        {
+            scrollTop: scrollTo.offset().top - topPicksScroll.offset().top + topPicksScroll.scrollTop(),
+        },
+        speed
+    );
 });
--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,39 +1,41 @@
 (function () {
-emailjs.init("user_05b6DcHp56rlXYPEUQP1y");
+    emailjs.init("user_05b6DcHp56rlXYPEUQP1y");
 })();
 
 function sendMail(contactForm) {
-    emailjs.send("gmail", "exploresiquijor", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            alert("Email Sent!", response);
-        },
-        function(error) {
-            alert("Failed to Send!", error);
-        });
-        return false;
+    emailjs
+        .send("gmail", "exploresiquijor", {
+            from_name: contactForm.name.value,
+            from_email: contactForm.emailaddress.value,
+            message: contactForm.message.value,
+        })
+        .then(
+            function (response) {
+                alert("Email Sent!", response);
+            },
+            function (error) {
+                alert("Failed to Send!", error);
+            }
+        );
+    return false;
 }
 
-function validate(email){
-        var emailRequiments = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+// Function to validate email text box (adjusted to suit my project- https://stackoverflow.com/questions/7635533/validate-email-address-textbox-using-javascript
+function validate(email) {
+    var emailRequirements = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-        if (emailRequiments.test(email.value) == false) 
-        {
-            alert('Invalid Email Address');
-            return false;
-        }
-        return true;
+    if (emailRequirements.test(email.value) == false) {
+        alert("Invalid Email Address");
+        return false;
+    }
+    return true;
 }
 
 // Function to reset value if input name/email & text area of message on submit (code altered to suit my project)- https://stackoverflow.com/questions/35492054/how-to-reset-input-field-jquery
-$(document).ready(function(){
-    $('form[name=contactForm]').on('submit', function() {
-          $('input[name=name]').val("")
-          $('input[name=emailaddress]').val("")
-          $('textarea[name=message]').val("")
-        });
+$(document).ready(function () {
+    $("form[name=contactForm]").on("submit", function () {
+        $("input[name=name]").val("");
+        $("input[name=emailaddress]").val("");
+        $("textarea[name=message]").val("");
+    });
 });
